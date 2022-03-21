1621025890c23302045f0f42e342454f728b6a9e
IngvarOlsen <ingvarolsen57@hotmail.com>
2022-03-21 23:52:09 +0100

    sqlCreateTable edit

---
+++ createTables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `Container` (
+    `ID` int  NOT NULL ,
+    `reciverSignalPin` int  NOT NULL ,
+    `loadingDate` datetime  NOT NULL ,
+    PRIMARY KEY (
+        `ID`
+    )
+);
+
+CREATE TABLE `Readings` (
+    `ID` int  NOT NULL ,
+    `temp` decimal  NOT NULL ,
+    `humid` decimal  NOT NULL ,
+    `containerID` int  NOT NULL ,
+    PRIMARY KEY (
+        `ID`
+    )
+);
+

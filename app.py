f2e6f05d3d2396e2691559dc093d2600b73a38ed
IngvarOlsen <ingvarolsen57@hotmail.com>
2022-03-25 01:03:55 +0100

    flask website first upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import sqlite3
+import sys
+import time
+from datetime import datetime
+from flask import Flask, url_for, render_template, request, g
+
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='template')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+########################
+##### Db handling ######
+########################
+
+# Had to make the db connector to a repeatable callable function, as the connection have to be close everytime it gets used 
+def dbConnect():
+    global conn
+    conn = sqlite3.connect('wegrain.db')
+    global curs
+    curs = conn.cursor()
+
+# Adds a dht22 reading
+# Parsing the input to avoid SQL injection
+def addReadings(temp, humid, containerId):
+    dbConnect()
+    sql = """INSERT INTO Readings(temp, humid, containerID) values(:temp, :humid, :containerId)"""
+    curs.execute(sql, [temp, humid, containerId])
+    conn.commit()
+    conn.close()
+
+# Adds a container
+def addContainer():
+    dbConnect()
+    sql = """INSERT INTO Container(loadingDate) values(:dateTime)"""
+    dateTime = datetime.now()
+    curs.execute(sql,[dateTime])
+    conn.commit()
+    conn.close()
+
+# return all readings
+def getAllReadings():
+    dbConnect()
+    for data in curs.execute("SELECT * FROM Readings;"):
+        print(data)  
+    conn.close()
+    return data
+
+# return inner join, gets all data belonging to specific container
+def getContainerReadings(containerId):
+    dbConnect()
+    sql = """SELECT * FROM Readings INNER JOIN Container ON :containerId = Container.ID"""
+    for data in curs.execute(sql, [containerId]):
+        print(data)  
+    conn.close()
+    return data
+
+# Deletes specifc container with ID
+def deleteContainer(containerId):
+    dbConnect()
+    sql = """DELETE FROM Container WHERE Container.ID = :containerId"""
+    curs.execute(sql, [containerId])
+    conn.commit()
+    conn.close()
+
+# Deletes specifc container with ID, and all readings of it
+def deleteContainerAndReadings(containerId):
+    dbConnect()
+    sql = """DELETE FROM Readings WHERE Readings.containerId = :containerId"""
+    curs.execute(sql, [containerId])
+    sql2 = """DELETE FROM Container WHERE Container.ID = :containerId"""
+    curs.execute(sql2, [containerId])
+    conn.commit()
+    conn.close()
+
+
+### For testing ###
+# getAllReadings()
+# addContainer()
+# addContainer()
+# addReadings(22, 22, 1)
+# addReadings(22, 23, 1)
+# addReadings(24, 25, 1)
+# addReadings(26, 28, 2)
+# #getContainerReadings(1)
+# #deleteContainerAndReadings(1)
+# conn.close()
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
